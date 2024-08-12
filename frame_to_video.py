04ccdec3fc4d8c58d2d9825a2e1c6682c9ece5b4
Vanhhh283 <136967494+Vanhhh283@users.noreply.github.com>
2024-08-12 18:58:45 +0700

    Add files via upload

---
+++ frame_to_video.py
@@ -0,0 +1,7 @@
+from ultis import *
+
+folder_path = 'D:/Xu ly anh/thucnghiem/v46_compare'  
+output_path = 'D:/Xu ly anh/thucnghiem/compare_video46_orb.mp4' 
+frame_duration = 0.2  #thời gian xuất hiện 1 ảnh
+
+create_video_from_frames(folder_path, output_path, frame_duration)
